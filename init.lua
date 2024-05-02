9709d1e6f8cd06bf5101202852287e0a7c1b9a48
Austin Miller <30607075+apmwebdev@users.noreply.github.com>
2024-05-02 01:04:26 -0700

    Set gruvbox contrast

--- init.lua
+++ init.lua
@@ -781,6 +781,9 @@ require('lazy').setup({
     -- 'folke/tokyonight.nvim',
     'ellisonleao/gruvbox.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
+    opts = {
+      contrast = 'hard',
+    },
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
